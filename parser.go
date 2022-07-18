2f581c896fb815ac9c966b758cc1c51d6613df67
0oM4R <omarksm09@gmail.com>
2022-07-18 22:49:24 +0200

    clean up empty lines in input

---
+++ parser.go
@@ -0,0 +1,23 @@
+package main 
+
+
+import "bufio"
+import "strings"
+
+var tmp string
+func cleanupInput(ini_input string) string {
+	scanner := bufio.NewScanner(strings.NewReader(ini_input))
+	var ini_WithNoComments string
+	for scanner.Scan() {
+	
+		tmp = scanner.Text()
+		if scanner.Text() == "" {
+			continue
+		}
+		tmp = strings.TrimPrefix(tmp," ")
+		ini_WithNoComments += tmp + "\n"
+		
+	}
+	ini_WithNoComments = strings.TrimSuffix(ini_WithNoComments, "\n")
+		return ini_WithNoComments
+}
---
+++ parser_test.go
@@ -0,0 +1,36 @@
+package main 
+import "testing"
+const expectedOut =`[DEFAULT]
+ServerAliveInterval = 45
+Compression = yes
+CompressionLevel = 9
+ForwardX11 = yes
+[bitbucket.org]
+User = hg
+[topsecret.server.com]
+Port = 50022
+ForwardX11 = no`
+func TestCleanUp(t *testing.T){
+	
+	t.Run("impty lines",func(t *testing.T){ 
+		var iniTemplate =`
+[DEFAULT]
+ServerAliveInterval = 45
+Compression = yes
+CompressionLevel = 9
+
+
+ForwardX11 = yes
+[bitbucket.org]
+
+User = hg
+[topsecret.server.com]
+Port = 50022
+ForwardX11 = no`
+	noComments:= cleanupInput(iniTemplate)
+	expected := expectedOut
+	if noComments != expected {
+		t.Errorf("expected '%s' but got '%s'", expected, noComments)
+	}
+})
+}
